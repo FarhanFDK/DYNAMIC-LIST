796f4fc3662a14d1da8e744113a7249919bb48f0
FarhanFDK <Farhanabdollahisd@gmail.com>
2020-09-13 10:49:22 -0400

    Details changed

---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php require "src/includes/head.php"; ?>
+<?php require "src/includes/header.php"; ?>
+<?php require "src/includes/middle.php"; ?>
+<?php require "src/includes/footer.php"; ?>
--- public/src/includes/head.php
+++ public/src/includes/head.php
@@ -1,5 +1,14 @@
+<!DOCTYPE html>
+<html>
 <head>
 	<meta charset="UTF-8" />
 	<title>DYNAMIC LIST</title>
 	<meta name="keywords" content="" />
+	<meta name="description" content="" />
+	<meta name="robots" content="nofollow,noindex" />
+	<meta name="viewport" content="width=device-width,initial-scale=1.0" />
+	<link rel="stylesheet" href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" />
+	<link rel="stylesheet" href="src/css/index.css" />
+	<link rel="stylesheet" href="src/css/media.css" />
+	<script src="https://kit.fontawesome.com/b39b75221a.js" crossorigin="anonymous"></script>
 </head>
